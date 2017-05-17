01ba1d10f73116bbbac361239eb4188de54755f2
Humberto Rocha <humrochagf@gmail.com>
2017-05-17 14:43:24 -0300

    matching flag styles with intl-tel-input and inproving more the dev pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,47 @@ var gulp        = require('gulp'),
     prefix      = require('gulp-autoprefixer'),
     notify      = require('gulp-notify'),
     cleanCSS    = require('gulp-clean-css'),
+    minify      = require('gulp-minify'),
+    rename      = require('gulp-rename'),
     http        = require('http'),
     ecstatic    = require('ecstatic'),
     browserSync = require('browser-sync');
 
-gulp.task('sass', function () {
+gulp.task('scss', function () {
   gulp.src('./src/scss/countrySelect.scss')
     .pipe(sass({ errLogToConsole: true }))
-    .pipe(prefix(['last 4 versions']))
+    .pipe(prefix())
+    .pipe(cleanCSS({compatibility: 'ie8', format: 'beautify', level: 0}))
     .pipe(gulp.dest('./build/css'))
     .pipe(notify("styles compiled"))
     .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('minify', function () {
-  gulp.src('./build/css/countrySelect.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(prefix(['last 4 versions']))
-    .pipe(gulp.dest('./build/css'))
-    .pipe(notify("styles minified"))
+gulp.task('js', function () {
+  gulp.src('./src/js/countrySelect.js')
+    .pipe(gulp.dest('./build/js'))
+    .pipe(notify("javascript updated"))
     .pipe(browserSync.reload({ stream: true }))
 });
+
+gulp.task('handle-sources', ['scss', 'js']);
+
+gulp.task('minify-scss', function () {
+  gulp.src('./build/css/countrySelect.css')
+    .pipe(cleanCSS({level: 2, inline: ['all']}))
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(notify("styles minified"))
+});
+
+gulp.task('minify-js', function () {
+  gulp.src('./build/js/countrySelect.js')
+    .pipe(minify({ext:{min:'.min.js'}}))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(notify("javascript minified"))
+});
+
+gulp.task('minify-sources', ['minify-scss', 'minify-js']);
 
 gulp.task('watch', function() {
   http.createServer(
@@ -31,13 +51,13 @@ gulp.task('watch', function() {
   ).listen(8080);
   browserSync({
     proxy: "0.0.0.0:8080",
-    files: ["./build/js/**/*.js"],
     ghostMode: false,
     notify: false,
   });
-  gulp.watch('./src/scss/**/*.scss', ['sass']);
+  gulp.watch('./src/scss/**/*.scss', ['scss']);
+  gulp.watch('./src/js/**/*.js', ['js']);
   gulp.watch("./**/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'watch']);
-gulp.task('build', ['sass', 'minify']);
+gulp.task('default', ['handle-sources', 'watch']);
+gulp.task('build', ['handle-sources', 'minify-sources']);
